1a01dfa6cf2a137ac76ae78e33e913414191dda4
Kadynskaya <olgakad9@gmail.com>
2021-03-01 12:45:43 +0200

    init: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
+  if(array && Array.isArray(array) && array.length !== 0) {
+    return Math.min(...array.map(item => item))
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if(array && Array.isArray(array) && array.length !== 0) {
+    return Math.max(...array.map(item => item))
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if(array && Array.isArray(array) && array.length !== 0) {
+    return array.reduce((sum, cur) => sum + cur) / array.length;
+  }
   return 0;
 }
